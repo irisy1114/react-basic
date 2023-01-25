28e5a83fc2ce43cba1688dd1d260e33be1e5dc4e
irisy <ayao@my.wctc.edu>
2023-01-25 09:53:22 -0600

    add country components and modified it

---
+++ src/components/Country.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Country extends Component {
+    state = {
+        name: 'United States',
+        gold: 0,
+    }
+
+    handleIncrement = () => {
+        this.setState((state) => ({ gold: state.gold + 1 }))
+    }
+
+    render() { 
+        return (
+            <div className="country">
+                <div className="name">
+                    { this.state.name }
+                </div>
+                <div className="medals">
+                    gold medals:  { this.state.gold }
+                    <button onClick={ this.handleIncrement }>+</button>
+                </div>
+            </div>
+        );
+      }
+}
+
+export default Country

0b2d51f09bba1b305ae552d7c101c3b5ec43d1ed
Iris Yao <91230673+irisy1114@users.noreply.github.com>
2023-02-23 17:51:55 -0600

    backend connect to my restful API

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 import Country from './components/Country';
 import NewCountry from './components/NewCountry';
 
 const App = () => {
   const [ countries, setCountries ] = useState([]);
+  // const apiEndpoint = "https://localhost:5001/api/country";
+  const apiEndpoint = "https://olympicmedals.azurewebsites.net/api/country";
 
   useEffect( () => {
-    let mutableCountries = [
-      { id: 1, name: 'United States', gold: 2 , silver: 2, bronze: 3 },
-      { id: 2, name: 'China', gold: 3, silver: 1, bronze: 0 },
-      { id: 3, name: 'Germany', gold: 0, silver: 2, bronze: 2 },  
-    ]
-    setCountries(mutableCountries);
+    async function fetchData() {
+      const { data: fetchedCountries } = await axios.get(apiEndpoint);
+      setCountries(fetchedCountries);
+    }
+    fetchData();
   }, []);
   const increaseMedalCount = (countryId, medalName) => {
     // look for country and medal name
-    const countriesMutable = [...countries];
-    const idx = countriesMutable.findIndex((c) => c.id === countryId)
-    countriesMutable[idx][medalName] += 1;
-    setCountries(countriesMutable);
+    // const countriesMutable = [...countries];
+    // const idx = countriesMutable.findIndex((c) => c.id === countryId)
+    // countriesMutable[idx][medalName] += 1;
+    // setCountries(countriesMutable);
   }
 
   const decreaseMedalCount = (countryId, medalName) => {
-    const countriesMutable = [...countries];
-    const idx = countriesMutable.findIndex((c) => c.id === countryId)
-    countriesMutable[idx][medalName] -= 1;
-    setCountries(countriesMutable);
+    // const countriesMutable = [...countries];
+    // const idx = countriesMutable.findIndex((c) => c.id === countryId)
+    // countriesMutable[idx][medalName] -= 1;
+    // setCountries(countriesMutable);
   }
 
   const getTotalMedalCount = () => {
-    // const countriesMutable = [...countries];
-    return countries.reduce((a, b) => a + b.gold + b.silver + b.bronze, 0);
+    // return countries.reduce((a, b) => a + b.gold + b.silver + b.bronze, 0);
   }
-  const addNewCountry = (name) => {
-    const id = countries.length === 0 ? 1 : Math.max(...countries.map(country => country.id)) + 1;
-    const mutableCountries = countries.concat({ id: id, name: name, gold: 0, silver: 0, bronze: 0 });
-    setCountries(mutableCountries);
+  const addNewCountry = async (countryName, gold, silver, bronze) => {
+    const { data: post } = await axios.post(apiEndpoint, { name: countryName, gold: gold, silver: silver, bronze: bronze});
+    setCountries(countries.concat(post));
   }
-  const deleteCountry = (countryId) => {
-    const mutableCountries = countries.filter(c => c.id !== countryId);
-    setCountries( mutableCountries);  
+  const deleteCountry = async (countryId) => {
+    const originalCountries = countries;
+    setCountries(countries.filter(c => c.id !== countryId));
+    try {
+      await axios.delete(`${apiEndpoint}/${countryId}`);
+    } catch(ex) {
+      if (ex.response && ex.response.status === 404) {
+        // word already deleted
+        console.log("The record does not exist - it may have already been deleted");
+      } else { 
+        alert('An error occurred while deleting a word');
+        setCountries(originalCountries);
+      }
+    }
   }
 
   return (

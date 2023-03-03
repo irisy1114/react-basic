26f763b889541cee50261adc55752a5e4a504e99
Iris Yao <91230673+irisy1114@users.noreply.github.com>
2023-03-02 21:54:51 -0600

    implement SignalR to the frontend

--- src/App.js
+++ src/App.js
@@ -1,21 +1,84 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import axios from 'axios';
+import { HubConnectionBuilder } from '@microsoft/signalr';
 import Country from './components/Country';
 import NewCountry from './components/NewCountry';
+import './App.css';
 
 const App = () => {
   const [countries, setCountries] = useState([]);
   const apiEndpoint = "https://localhost:5001/api/country";
+  const hubEndpoint = "https://localhost:5001/medalsHub"
   // const apiEndpoint = "https://olympicmedals.azurewebsites.net/api/country";
+  // const hubEndpoint = "https://medalsapi.azurewebsites.net/medalsHub"
+  const [ connection, setConnection] = useState(null);
+  // const medals = useRef([
+  //   { id: 1, name: 'gold' },
+  //   { id: 2, name: 'silver' },
+  //   { id: 3, name: 'bronze' },
+  // ]);
 
+  const latestCountries = useRef(null);
+  // latestCountries.current is a ref variable to countries
+  // this is needed to access state variable in useEffect w/o dependency
+  latestCountries.current = countries;
+
+  // this is the functional equivalent to componentDidMount
   useEffect(() => {
+    // initial data loaded here
     async function fetchData() {
       const { data: fetchedCountries } = await axios.get(apiEndpoint);
       setCountries(fetchedCountries);
     }
     fetchData();
+
+    // signalR
+    const newConnection = new HubConnectionBuilder()
+      .withUrl(hubEndpoint)
+      .withAutomaticReconnect()
+      .build();
+
+    setConnection(newConnection);
   }, []);
+
+  // componentDidUpdate (changes to connection)
+  useEffect(() => {
+    if (connection) {
+      connection.start()
+      .then(() => {
+        console.log('Connected!')
+
+        connection.on('ReceiveAddMessage', country => {
+          console.log(`Add: ${country.name}`);
+          let mutableCountries = [...latestCountries.current];
+          mutableCountries = mutableCountries.concat(country);
+
+          setCountries(mutableCountries);
+        });
+
+        connection.on('ReceiveDeleteMessage', id => {
+          console.log(`Delete id: ${id}`);
+          let mutableCountries = [...latestCountries.current];
+          mutableCountries = mutableCountries.filter(c => c.id !== id);
+
+          setCountries(mutableCountries);
+        });
+
+        connection.on('ReceivePatchMessage', country => {
+          console.log(`Patch: ${country.name}`);
+          let mutableCountries = [...latestCountries.current];
+          const idx = mutableCountries.findIndex(c => c.id === country.id);
+          mutableCountries[idx] = country;
+
+          setCountries(mutableCountries);
+        });
+      })
+      .catch(e => console.log('Connection failed: ', e));
+    }
+  // useEffect is dependent on changes connection
+  }, [connection]);
+
   const increaseMedalCount = (countryId, medalName) => handleUpdate(countryId, medalName, 1);
   const decreaseMedalCount = (countryId, medalName) => handleUpdate(countryId, medalName, -1)
   const handleUpdate = async (countryId, medalName, factor) => {
@@ -43,9 +106,8 @@ const App = () => {
     const getTotalMedalCount = () => {
       return countries.reduce((a, b) => a + b.gold + b.silver + b.bronze, 0);
     }
-    const addNewCountry = async (countryName, gold, silver, bronze) => {
-      const { data: post } = await axios.post(apiEndpoint, { name: countryName, gold: gold, silver: silver, bronze: bronze });
-      setCountries(countries.concat(post));
+    const addNewCountry = async (name) => {
+      await axios.post(apiEndpoint, { name: name});
     }
     const deleteCountry = async (countryId) => {
       const originalCountries = countries;
@@ -57,7 +119,7 @@ const App = () => {
           // word already deleted
           console.log("The record does not exist - it may have already been deleted");
         } else {
-          alert('An error occurred while deleting a word');
+          alert('An error occurred while deleting');
           setCountries(originalCountries);
         }
       }
